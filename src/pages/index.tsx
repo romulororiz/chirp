f3967f65f6efd42b87e95470a658575cb6f914a0
Romulo Roriz <romulonmroriz@gmail.com>
2023-04-02 13:15:21 +0200

    Add profile and post routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,7 @@ import { LoadingPage, LoadingSpinner } from "~/components/Loading";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { z } from "zod";
+import Link from "next/link";
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
@@ -105,19 +106,25 @@ const PostsView = (props: PostWithUser) => {
       key={post.id}
       className="flex items-center gap-3 border-b border-slate-400 p-4"
     >
-      <Image
-        src={user.profileImageUrl}
-        alt={`@${user.username}`}
-        className="h-14 w-14 rounded-full"
-        width={56}
-        height={56}
-      />
+      <Link href={`/@${user.username}`}>
+        <Image
+          src={user.profileImageUrl}
+          alt={`@${user.username}`}
+          className="h-14 w-14 rounded-full"
+          width={56}
+          height={56}
+        />
+      </Link>
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
-          <span>{`@${user.username}`}</span>{" "}
-          <span className="font-thin">{`· ${dayjs(
-            post.createdAt
-          ).fromNow()}`}</span>
+          <Link href={`/@${user.username}`}>
+            <span>{`@${user.username}`}</span>
+          </Link>{" "}
+          <Link href={`/post/${post.id}`}>
+            <span className="font-thin">{`· ${dayjs(
+              post.createdAt
+            ).fromNow()}`}</span>
+          </Link>
         </div>
         <span className="text-2xl">{post.content}</span>
       </div>
